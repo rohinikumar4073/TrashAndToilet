bc637fbc47c17a1778643ec28aa0876d1d3e083f
Rohini Kumar <vissu_nbkr@yahoo.com>
2013-02-19 21:41:58 +0530

    Commited by rohini

---
+++ src/com/trashandtoilet/SearchToilet.java
@@ -0,0 +1,82 @@
+package com.trashandtoilet;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.trashandtoilet.service.GPSTracker;
+
+@SuppressLint("NewApi")
+public class SearchToilet extends FragmentActivity /*
+													 * implements'
+													 * OnMapClickListener,
+													 * OnMapLongClickListener,
+													 * OnCameraChangeListener
+													 */{
+	static final LatLng FirstToilet = new LatLng(17.447729806707645,
+			78.3633230254054);
+	static final LatLng SecondToilet = new LatLng(17.4438208734482,
+			78.36638074368238);
+	private GoogleMap map;
+
+	@SuppressLint("NewApi")
+	protected void onCreate(Bundle savedInstanceState) {
+		System.out.println("Starting Search");
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_search_toilet);
+		GPSTracker gps = new GPSTracker(SearchToilet.this);
+		// check if GPS enabled
+		if (gps.canGetLocation()) {
+
+			double latitude = gps.getLatitude();
+			double longitude = gps.getLongitude();
+			CameraPosition cLocation = new CameraPosition.Builder()
+					.target(new LatLng(latitude, longitude)).zoom(15.5f)
+					.bearing(300).tilt(50).build();
+
+			map = ((SupportMapFragment) getSupportFragmentManager()
+					.findFragmentById(R.id.map)).getMap();
+
+			map.moveCamera(CameraUpdateFactory.newCameraPosition(cLocation));
+			map.addMarker(new MarkerOptions().position(
+					new LatLng(latitude, longitude)).title("Marker"));
+			addToiletsAndDustbins(map);
+			/*
+			 * map.setOnMapClickListener(this);
+			 * map.setOnMapLongClickListener(this);
+			 * map.setOnCameraChangeListener(this);
+			 */
+			// \n is for new line
+		} else {
+			// can't get location
+			// GPS or Network is not enabled
+			// Ask user to enable GPS/network in settings
+			gps.showSettingsAlert();
+		}
+
+	}
+
+	private void addToiletsAndDustbins(GoogleMap map) {
+		map.addMarker(new MarkerOptions().position(FirstToilet).title("Marker"));
+		map.addMarker(new MarkerOptions().position(SecondToilet)
+				.title("Marker"));
+
+	}
+
+	/*
+	 * @Override public void onMapClick(LatLng point) {
+	 * System.out.println("tapped, point=" + point); }
+	 * 
+	 * @Override public void onMapLongClick(LatLng point) { count++;
+	 * System.out.println(count+" long pressed, point=" + point); }
+	 * 
+	 * @Override public void onCameraChange(final CameraPosition position) {
+	 * System.out.println(position.toString()); } public static int count=0;
+	 */
+}
---
+++ src/com/trashandtoilet/SearchTrash.java
@@ -0,0 +1,50 @@
+package com.trashandtoilet;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.trashandtoilet.service.GPSTracker;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class SearchTrash extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_search_trash);
+	
+		
+		// Show the Up button in the action bar.
+		//getActionBar().setDisplayHomeAsUpEnabled(true);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_search_trash, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			// This ID represents the Home or Up button. In the case of this
+			// activity, the Up button is shown. Use NavUtils to allow users
+			// to navigate up one level in the application structure. For
+			// more details, see the Navigation pattern on Android Design:
+			//
+			// http://developer.android.com/design/patterns/navigation.html#up-vs-back
+			//
+			NavUtils.navigateUpFromSameTask(this);
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+}
